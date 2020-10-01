a67e01aa2f55a147caa4a3f3e8edfef5ceaea6ef
Hailey Strobelt <haileystrobelt@gmail.com>
2020-09-30 23:16:48 -0600

    Creating Projects

---
+++ CharacterMoverProject/Assets/Scripts/WalkRun.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class WalkRun : MonoBehaviour
+{
+
+    public Rigidbody rigBod;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Dash"))
+            {
+                
+            }
+    }
+}
