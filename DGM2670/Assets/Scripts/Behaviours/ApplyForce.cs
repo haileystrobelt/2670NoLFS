611b69f9cccdde9331ffdffe3f684282924a41a8
Hailey Strobelt <haileystrobelt@gmail.com>
2020-10-08 18:59:09 -0600

    Unity Edits

--- DGM2670/Assets/Scripts/Behaviours/ApplyForce.cs
+++ DGM2670/Assets/Scripts/Behaviours/ApplyForce.cs
@@ -4,17 +4,13 @@
 public class ApplyForce : MonoBehaviour
 {
     private Rigidbody rBody;
-    
     public float force = 30f;
-    public Vector3 playerRotation;
     
     
     void Start()
     {
         rBody = GetComponent<Rigidbody>();
-        
         var forceDirection = new Vector3(force, 0, 0);
-        //forceDirection needs to be base on Player rotation (Use Scriptable Object)
         rBody.AddRelativeForce(forceDirection);
     }
 
