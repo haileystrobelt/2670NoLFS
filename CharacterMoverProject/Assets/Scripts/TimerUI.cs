ad75ecde44b2e9052d1a8a3cf67d995311a63d92
Hailey Strobelt <haileystrobelt@gmail.com>
2020-10-12 16:59:00 -0600

    Unity Edits

---
+++ CharacterMoverProject/Assets/Scripts/TimerUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class TimerUI : MonoBehaviour
+{
+    public int maxTime;
+    public IntData time;
+    public Text timerText;
+    
+    void Start()
+    {
+        time.value = maxTime;
+    }
+
+    public IEnumerator Countdown()
+    {
+        time.value = maxTime;
+        while (time.value >= 0)
+        {
+            DisplayTimer();
+            yield return new WaitForSeconds(1.0f);
+            time.value--;
+        }
+    }
+
+    public void DisplayTimer()
+    {
+        timerText.text = time.value.ToString();
+    }
+}
---
+++ CharacterMoverProject/Assets/Scripts/TriggerBehaviour.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+
+public class TriggerBehaviour : MonoBehaviour
+{
+    private MeshRenderer meshR;
+    public Color newColor;
+    public Color defaultColor;
+    
+    
+    private Shader shader1;
+    private Renderer rend;
+
+    private WaitForSeconds wfs;
+    public int holdTime = 10;
+    
+    public GameObject door;
+    public TimerUI timer;
+    
+    
+    void Start()
+    {
+        meshR = GetComponent<MeshRenderer>();
+        meshR.material.color = defaultColor;
+        
+        shader1 = Shader.Find("Transparent/Diffuse");
+        rend = GetComponent<Renderer>();
+        
+        wfs = new WaitForSeconds(holdTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        newColor.a = 0.5f; 
+        meshR.material.color = newColor;
+        
+        rend.material.shader = shader1;
+        
+        door.SetActive(false);
+
+        
+    }
+
+    private IEnumerator OnTriggerExit(Collider other)
+    {
+        StartCoroutine(timer.Countdown()); 
+        yield return wfs;
+        newColor.a = 1f;
+        meshR.material.color = defaultColor;
+        
+        door.SetActive(true);
+        
+    }
+}
