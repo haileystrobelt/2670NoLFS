a7d90768ca84583c2f53f774d717ea4e93a9f980
Hailey Strobelt <haileystrobelt@gmail.com>
2020-10-22 21:16:21 -0600

    Unity Edits

---
+++ DGM2670/Assets/Scripts/Behaviours/CharacterWithStates.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class CharacterWithStates : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 movement;
+    
+    public CharacterStateMachineData characterStates;
+
+    
+    public float moveSpeed = 3;
+    public float gravity = -9.81f;
+        
+        
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        var newInput = Input.GetAxis("Vertical")*moveSpeed;
+                
+        switch (characterStates.value)
+        {
+            case CharacterStateMachineData.characterStates.StandardWalk:
+                movement.Set(newInput,gravity,0);
+                print("Walk");
+                break;
+            case CharacterStateMachineData.characterStates.NoGravityWalk:
+                movement.Set(newInput,0,0);
+                print("Walk");
+                break;
+            case CharacterStateMachineData.characterStates.WallCrawl:
+                movement.Set(0,newInput,0);
+                print("Crawl");
+                break;
+            case CharacterStateMachineData.characterStates.KnockBack:
+                print("KnockBack");
+                break;
+        }
+
+        var newMovement = movement * Time.deltaTime;
+        controller.Move(newMovement);
+    }
+}
