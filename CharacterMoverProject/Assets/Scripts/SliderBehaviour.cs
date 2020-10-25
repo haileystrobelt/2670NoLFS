556e0f80a0211a6c0ad475e64570d7bd94e0849a
Hailey Strobelt <haileystrobelt@gmail.com>
2020-10-24 23:52:37 -0600

    Unity Edits

---
+++ CharacterMoverProject/Assets/Scripts/SliderBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+[RequireComponent(typeof(Slider))]
+
+public class SliderBehaviour : MonoBehaviour
+{
+    private Slider healthSlider;
+    private RectTransform rectTransform;
+    public FloatData health, maxHealth;
+    
+    
+
+    private void Start()
+    {
+        healthSlider = GetComponent<Slider>();
+        healthSlider.maxValue = maxHealth.value;
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        healthSlider.value = health.value;
+        var newSize = new Vector2(maxHealth.value, rectTransform.sizeDelta.y);
+        rectTransform.sizeDelta = newSize;
+    }
+}
