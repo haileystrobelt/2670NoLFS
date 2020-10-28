2604400abb1470cdc7a0b282d570cea36d313e54
Hailey Strobelt <haileystrobelt@gmail.com>
2020-10-27 23:55:45 -0600

    Unity Edits

---
+++ DGM2670/Assets/Scripts/Behaviours/InvokeBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class InvokeBehaviour : MonoBehaviour
+{
+    public UnityEvent OnInvokeEvent;
+    public float holdTime = 3f;
+    
+    public void OnInvoke()
+    {
+        Invoke(nameof(InvokeCall), holdTime);      
+    }
+
+    private void InvokeCall()
+    {
+        OnInvokeEvent.Invoke();
+    }
+}
