0131a10a449e45f0fde6cc51502a9feda7ebe7b7
Hailey Strobelt <haileystrobelt@gmail.com>
2020-10-30 14:58:42 -0600

    Unity Edits

--- DGM2670/Assets/Scripts/Behaviours/MonoEventsBehaviour.cs
+++ DGM2670/Assets/Scripts/Behaviours/MonoEventsBehaviour.cs
@@ -1,17 +1,41 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.Events;
 
 public class MonoEventsBehaviour : MonoBehaviour
 {
-    public UnityEvent startEvent, onEnableEvent;
+       public UnityEvent awakeEvent, startEvent, onEnableEvent, onDisableEvent;
+       public float holdTime = 1f;
+       public bool repeatOnStart;
 
-    private void Start()
-    {
-        startEvent.Invoke();
-    }
+       private void Awake()
+       {
+              awakeEvent.Invoke();
+       }
 
-    private void OnEnable()
-    {
-        onEnableEvent.Invoke();
-    }
+       public void Restart()
+       {
+              StartCoroutine(Start());
+       }
+       
+       private IEnumerator Start()
+       {
+              yield return new WaitForSeconds(holdTime);
+              startEvent.Invoke();
+
+              while (repeatOnStart)
+              { 
+                     yield return new WaitForSeconds(holdTime);
+                     startEvent.Invoke();
+              }
+       }
+       private void OnEnable()
+       {
+              onEnableEvent.Invoke();
+       }
+
+       private void OnDisable()
+       {
+              onEnableEvent.Invoke();
+       }
 }
