81e20a2a6b49f293387c7bbc383427d7274f5e6d
Hailey <haileystrobelt@gmail.com>
2020-11-20 21:22:55 -0700

    Unity Edits

--- DGM2670/Assets/Scripts/Behaviours/GameActionHandler.cs
+++ DGM2670/Assets/Scripts/Behaviours/GameActionHandler.cs
@@ -5,13 +5,11 @@ public class GameActionHandler : MonoBehaviour
 {
     public GameAction gameAction;
     public UnityEvent handlerEvent;
-    public float holdTime = 3f;
-    
+    public float holdTime;
     private void Start()
     {
         gameAction.action += ActionHandler;  
     }
-
     private void ActionHandler()
     {
         Invoke(nameof(OnActionHandler), holdTime);
---
+++ DGM2670/Assets/Scripts/Behaviours/NavAgentBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavAgentBehaviour : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Transform destination;
+    public GameAction getTransformAction, callForTransformAction;
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        destination = transform;
+        getTransformAction.transformAction += HandleTransform;
+        callForTransformAction.action?.Invoke();
+    }
+
+    private void HandleTransform(Transform obj)
+    {
+        destination = obj;
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        destination = other.transform;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        destination = transform;
+    }
+
+    private void Update()
+    {
+        agent.destination = destination.position;
+    }
+}
