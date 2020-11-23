4c3aca00bad89214d1983540b997467ae3eea8c5
Hailey <haileystrobelt@gmail.com>
2020-11-22 21:22:58 -0700

    Unity Edits

--- DGM2670/Assets/Scripts/ScriptableObjects/GameAction.cs
+++ DGM2670/Assets/Scripts/ScriptableObjects/GameAction.cs
@@ -2,12 +2,18 @@
 using UnityEngine.Events;
 
 [CreateAssetMenu]
-
 public class GameAction : ScriptableObject
 {
     public UnityAction action;
+    public UnityAction<Transform> transformAction;
     public void Raise()
     {
         action?.Invoke();
+    }
+
+    public void Raise(Transform obj)
+    {
+        Debug.Log(obj);
+        transformAction(obj);
     }
 }
