74e3db9423f14e14bd7b45cc3de48398dcea09af
Hailey <haileystrobelt@gmail.com>
2020-11-26 11:00:15 -0700

    Unity Edits

---
+++ DGM2670/Assets/Scripts/Behaviours/WizardBrain.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu]
+public class WizardBrain : AiBrainBase
+{
+    public override void Navigate(NavMeshAgent agent)
+    {
+        base.Navigate(agent);
+        //Do what Wizards Do
+    }
+}
